6708d16aa3d82e73a98d7cb439fef93d2a00954d
98414 <984149293@qq.com>
2024-09-27 08:48:43 +0800

    2024-9-25 snow sword

--- src/main/java/com/soybean/enchantment/WaterGoddessEnchantment.java
+++ src/main/java/com/soybean/enchantment/WaterGoddessEnchantment.java
@@ -48,13 +48,13 @@ public class WaterGoddessEnchantment extends Enchantment {
     }
 
     public void BlockToWater(LivingEntity entity, World world, BlockPos blockPos, int level) {
-        if(!world.isClient){
+        if (!world.isClient) {
             BlockState blockState = Blocks.WATER.getDefaultState();
             int i = 2 + level;
-            int maxY = Math.min(8 , i / 2);
+            int maxY = Math.min(8, i / 2);
             BlockPos.Mutable mutable = new BlockPos.Mutable();
             Iterator var7;
-            var7 = BlockPos.iterate(blockPos.add(-i, - maxY, -i), blockPos.add(i, maxY, i)).iterator();
+            var7 = BlockPos.iterate(blockPos.add(-i, -maxY, -i), blockPos.add(i, maxY, i)).iterator();
             while (var7.hasNext()) {
                 BlockPos blockPos2 = (BlockPos) var7.next();
                 if (blockPos2.isWithinDistance(entity.getPos(), (double) i)) {
@@ -73,41 +73,43 @@ public class WaterGoddessEnchantment extends Enchantment {
             if (world.isClient) return ActionResult.PASS;
             ItemStack mainHand = player.getStackInHand(hand);
             int level = EnchantmentHelper.getLevel(Enchantseries.WATER_GODDESS_ENCHANTMENT, mainHand);
-            // 判断物品是否为可以放置的方块 (BlockItem)
-            Block block = Blocks.WATER;
-            // 检查主手物品是否具有耐久度
-            if (mainHand.isDamageable()) {
-                // 减少主手物品的耐久度
-                mainHand.damage(1, player, (p) -> {
-                    p.sendToolBreakStatus(hand);  // 处理工具破损
-                });
-                // 将击中的方块替换为 lastStack 中的方块
-                BlockState newState = block.getDefaultState();
-                BlockPos.Mutable mutable = new BlockPos.Mutable();
-                Iterator var7;
-                if (level == 1) {
-                    world.setBlockState(pos, newState);
-                    // 播放方块放置的音效
-                    world.playSound(null, pos, block.getSoundGroup(newState).getPlaceSound(), SoundCategory.BLOCKS, 1.0F, 1.0F);
-                } else {
-                    int range = level - 1;
-                    var7 = BlockPos.iterate(pos.add(-range, Math.max(-8, -range), -range), pos.add(range, Math.min(8, range), range)).iterator();
-                    while (var7.hasNext()) {
-                        BlockPos blockPos2 = (BlockPos) var7.next();
-                        if (blockPos2.isWithinDistance(pos, (double) range)) {
-                            mutable.set(blockPos2.getX(), blockPos2.getY() + 1, blockPos2.getZ());
-                            BlockState blockState3 = world.getBlockState(blockPos2);
-                            if (blockState3 != Blocks.AIR.getDefaultState() && newState.canPlaceAt(world, blockPos2) && world.canPlace(newState, blockPos2, ShapeContext.absent())) {
-                                world.setBlockState(blockPos2, newState);
-                                world.playSound(null, pos, block.getSoundGroup(newState).getPlaceSound(), SoundCategory.BLOCKS, 1.0F, 1.0F);
+            if (level > 0) {
+                // 判断物品是否为可以放置的方块 (BlockItem)
+                Block block = Blocks.WATER;
+                // 检查主手物品是否具有耐久度
+                if (mainHand.isDamageable()) {
+                    // 减少主手物品的耐久度
+                    mainHand.damage(1, player, (p) -> {
+                        p.sendToolBreakStatus(hand);  // 处理工具破损
+                    });
+                    // 将击中的方块替换为 lastStack 中的方块
+                    BlockState newState = block.getDefaultState();
+                    BlockPos.Mutable mutable = new BlockPos.Mutable();
+                    Iterator var7;
+                    if (level == 1) {
+                        world.setBlockState(pos, newState);
+                        // 播放方块放置的音效
+                        world.playSound(null, pos, block.getSoundGroup(newState).getPlaceSound(), SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    } else {
+                        int range = level - 1;
+                        var7 = BlockPos.iterate(pos.add(-range, Math.max(-8, -range), -range), pos.add(range, Math.min(8, range), range)).iterator();
+                        while (var7.hasNext()) {
+                            BlockPos blockPos2 = (BlockPos) var7.next();
+                            if (blockPos2.isWithinDistance(pos, (double) range)) {
+                                mutable.set(blockPos2.getX(), blockPos2.getY() + 1, blockPos2.getZ());
+                                BlockState blockState3 = world.getBlockState(blockPos2);
+                                if (blockState3 != Blocks.AIR.getDefaultState() && newState.canPlaceAt(world, blockPos2) && world.canPlace(newState, blockPos2, ShapeContext.absent())) {
+                                    world.setBlockState(blockPos2, newState);
+                                    world.playSound(null, pos, block.getSoundGroup(newState).getPlaceSound(), SoundCategory.BLOCKS, 1.0F, 1.0F);
+                                }
                             }
                         }
                     }
+                    // 成功修改方块，返回 ActionResult.SUCCESS
+                    return ActionResult.SUCCESS;
                 }
-                // 成功修改方块，返回 ActionResult.SUCCESS
-                return ActionResult.SUCCESS;
             }
-        return ActionResult.PASS;
-    });
-}
+            return ActionResult.PASS;
+        });
+    }
 }
